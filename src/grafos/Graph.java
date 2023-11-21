b4f5846084f324db3620cd21cebbddf51e6a69bc
Matheus Franco <matheusefranco@gmail.com>
2023-11-21 10:42:12 -0300

    Menu

--- src/grafos/Graph.java
+++ src/grafos/Graph.java
@@ -1,9 +1,12 @@
 
 package grafos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
-
-
 
 public class Graph {
      private Map<Integer, LinkedList<Aresta>> listaAdjacencia;
@@ -24,6 +27,8 @@ public class Graph {
         }
 
         listaAdjacencia.get(origem).add(new Aresta(destino, peso));
+        //listaAdjacencia.get(destino).add(new Aresta(origem, peso));
+
     }
     
   
@@ -38,6 +43,38 @@ public class Graph {
             System.out.println();
         }
     }
+    
+     public void salvarGrafo() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("grafo.txt"))) {
+            for (Map.Entry<Integer, LinkedList<Aresta>> entry : listaAdjacencia.entrySet()) {
+                int vertice = entry.getKey();
+                LinkedList<Aresta> vizinhos = entry.getValue();
+                for (Aresta vizinho : vizinhos) {
+                    writer.println(vertice + " " + vizinho.vertice + " " + vizinho.peso);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para carregar os dados do grafo de um arquivo texto
+    public void carregarGrafo() {
+        try (Scanner scanner = new Scanner(new File("grafo.txt"))) {
+            while (scanner.hasNext()) {
+                int origem = scanner.nextInt();
+                int destino = scanner.nextInt();
+                int peso = scanner.nextInt();
+
+                adicionarVertice(origem);
+                adicionarVertice(destino);
+                adicionarAresta(origem, destino, peso);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     
   
     
